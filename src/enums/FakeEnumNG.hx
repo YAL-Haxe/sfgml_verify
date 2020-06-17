01978644cc275219077c118be06fd89fe83b9350
YellowAfterlife <yellowafterlife@hotmail.com>
2020-06-17 16:53:09 +0300

    Added a test for a @:doc fake enum

--- src/enums/FakeEnumNG.hx
+++ src/enums/FakeEnumNG.hx
@@ -1,5 +1,31 @@
 package enums;
+
+#if !macro
 import VerifyTools.*;
+#end
+
+import haxe.macro.Expr;
+class Macro {
+	public static macro function test(e:Expr) {
+		return macro {
+			var f = $e.A;
+			f = $e.C;
+			switch (f) {
+				case C: {};
+				case B: assert(false, "match");
+				default: assert(false, "match");
+			};
+			assertEq(f.getIndex(), 2, ".getIndex");
+			assertEq(f.getName(), "C", ".getName");
+			assertEq(Type.enumIndex(f), 2, "Type.getIndex");
+			assertEq(Type.enumConstructor(f), "C", "Type.enumConstructor");
+			assertArrayEq(Type.enumParameters(f), [], "Type.enumParameters");
+			assertEq(Type.createEnum($e, "C"), C, "Type.createEnum");
+			assertEq(Type.createEnumIndex($e, 2), C, "Type.createEnumIndex");
+			assertArrayEq(Type.getEnumConstructs($e), ["A", "B", "C", "D"], "Type.getEnumConstructs");
+		};
+	}
+}
 /**
 	Fake (parameter-less) enums should be compiled straight to their indexes.
 	We will also replace most Type.* helpers with direct access if possible.
@@ -11,31 +37,25 @@ import VerifyTools.*;
 	]
 	@requiredCode [
 		"enum enums_FN {",
-		"FN.A"
+		"enums_FN.A",
+		"enums_DE_A"
 	]
 	@forbiddenCode globalvar mt_
 	@author YellowAfterlife
 **/
 class FakeEnumNG {
 	public static inline function main() {
-		var f:FN = FN.A;
-		f = FN.C;
-		switch (f) {
-			case C: {};
-			case B: assert(false, "match");
-			default: assert(false, "match");
-		};
-		assertEq(f.getIndex(), 2, ".getIndex");
-		assertEq(f.getName(), "C", ".getName");
-		assertEq(Type.enumIndex(f), 2, "Type.getIndex");
-		assertEq(Type.enumConstructor(f), "C", "Type.enumConstructor");
-		assertArrayEq(Type.enumParameters(f), [], "Type.enumParameters");
-		assertEq(Type.createEnum(FN, "C"), C, "Type.createEnum");
-		assertEq(Type.createEnumIndex(FN, 2), C, "Type.createEnumIndex");
-		assertArrayEq(Type.getEnumConstructs(FN), ["A", "B", "C", "D"], "Type.getEnumConstructs");
+		Macro.test(FN);
+		Macro.test(DE);
 	}
 }
 @:nativeGen enum FN {
+	A;
+	B;
+	C;
+	D;
+}
+@:doc enum DE {
 	A;
 	B;
 	C;
